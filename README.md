235023261207d2c008d809ba53e9ed721a30c80e
backloghero-lang <d.maslyk@onet.eu>
2026-06-27 15:46:17 +0200

    Bourbon Hunters - PWA + REST API (Gemini + Google Search)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-  <img src="banner.png" alt="Bourbon Hunters" width="100%">
+  <img src="assets/readme/flaming-logo.png" alt="Bourbon Hunters flaming logo" width="420">
 </p>
 
 <p align="center">
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Bourbon Hunters service worker - network-first dla aktualizacji aplikacji i bazy. */
-const CACHE = "bourbon-hunters-v13";
+const CACHE = "bourbon-hunters-v14";
 const ASSETS = [
   "./",
   "./index.html",
