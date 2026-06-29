cd10cd5f420e080cd4396abeb0c8a7db3ca91cb1
backloghero-lang <d.maslyk@onet.eu>
2026-06-29 16:45:22 +0200

    Bourbon Hunters - PWA + REST API (Gemini + Google Search)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Bourbon Hunters service worker - network-first dla aktualizacji aplikacji i bazy. */
-const CACHE = "bourbon-hunters-v33";
+const CACHE = "bourbon-hunters-v34";
 const ASSETS = [
   "./",
   "./index.html",
