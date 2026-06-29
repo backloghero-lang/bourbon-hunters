883c1db4311eac93e319b5d780f3ca529b6302c6
backloghero-lang <d.maslyk@onet.eu>
2026-06-29 19:31:33 +0200

    Update Bourbon Hunters

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Bourbon Hunters service worker - network-first dla aktualizacji aplikacji i bazy. */
-const CACHE = "bourbon-hunters-v42";
+const CACHE = "bourbon-hunters-v43";
 const ASSETS = [
   "./",
   "./index.html",
